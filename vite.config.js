7b9d1a1f5b291ab4c50e85f2b3c69ac68f9f8f23
anilmikkili <46949448+amikkili@users.noreply.github.com>
2026-04-03 09:15:25 -0700

    Add comment for Vite configuration reference

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// https://vite.dev/
 export default defineConfig({
   plugins: [react()],
 })
